03e81a0b84e1be7fe0cb9a08f84be574b1f2486b
Rustam Kudratov <58280683+R-Kudratov@users.noreply.github.com>
2025-09-26 00:06:43 +0300

    chore: Добавил шаблон приложения из задания

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,28 @@
 import './App.css'
 
-export function App() {}
+import { useFetch } from './hooks/useFetch'
+
+export function App() {
+  const { data, isLoading, error, refetch } = useFetch('https://jsonplaceholder.typicode.com/posts')
+
+  return (
+    <div>
+      <div>
+        <button
+          onClick={() =>
+            refetch({
+              params: {
+                _limit: 3,
+              },
+            })
+          }
+        >
+          Перезапросить
+        </button>
+      </div>
+      {isLoading && 'Загрузка...'}
+      {error && 'Произошла ошибка'}
+      {data && !isLoading && data.map((item) => <div key={item.id}>{item.title}</div>)}
+    </div>
+  )
+}
