6ad9d25a9a54a38af1e1cead61f3cd9919defce2
Rustam Kudratov <58280683+R-Kudratov@users.noreply.github.com>
2025-09-27 21:42:11 +0300

    chore: Добавил шаблон приложения из задания

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
 import './App.css'
 
-export function App() {}
+import { useLocalStorage } from './hooks/useLocalStorage'
+
+export function App() {
+  const [value, { setItem, removeItem }] = useLocalStorage('some-key')
+
+  return (
+    <div>
+      <p>Значение из LocalStorage: {value}</p>
+      <div>
+        <button onClick={() => setItem('new storage value')}>Задать значение</button>
+        <button onClick={() => removeItem()}>Удалить значение</button>
+      </div>
+    </div>
+  )
+}
